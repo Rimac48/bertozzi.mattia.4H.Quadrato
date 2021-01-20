e0a731e11838599af97ae4ebf627377c52eee5c3
Rimac48 <mattia.bertozzi@live>
2021-01-20 10:03:49 +0100

    Terzo Commit

---
+++ bertozzi.mattia.4h.Quadrato/Models/Quadrato.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace bertozzi.mattia._4h.Quadrato.Models
+{
+    class Q
+    {
+        private int _lposx;
+        private int _lposy;
+
+        //costruttore
+        public Q(int x, int y, double l)
+        {
+            _lposx = x;
+            _lposy = y;
+            _lato = l;
+        }
+
+        public int LposX
+        {
+            get
+            {
+                return _lposx;
+            }
+            set
+            {
+                _lposx = value;
+            }
+        }
+
+        public int LposY
+        {
+            get
+            {
+                return _lposy;
+            }
+            set
+            {
+                _lposy = value;
+            }
+        }
+
+        private double _lato;
+
+        public double Lato
+        {
+            get
+            {
+                return _lato;
+            }
+            set
+            {
+                _lato = value;
+            }
+        }
+
+        private double _area;
+
+        public double Area
+        {
+            get
+            {
+                return _area;
+            }
+            set
+            {
+                _area = value;
+            }
+        }
+
+        private double _perimetro;
+
+        public double Perimetro
+        {
+            get
+            {
+                return _perimetro;
+            }
+            set
+            {
+                _perimetro = value;
+            }
+        }
+
+        //METODI
+
+        //calcolo dell'area
+        public double CArea()
+        {
+            Area = _lato * _lato;
+            return Area;
+        }
+
+        //calcolo del perimetro
+        public double CPerimetro()
+        {
+            Perimetro = _lato * 4;
+            return Perimetro;
+        }
+
+        //calcolo della diagonale
+        public double CDiagonale()
+        {
+            return _lato * Math.Sqrt(2);
+        }
+
+        //metodo dell'output del punto
+        public string OutPunto()
+        {
+            return $"Punto({_lposx},{_lposy})";
+        }
+
+        //Overload del Segno ">"
+        public static bool operator >(Q Q1, Q Q2)
+            {
+                if(Q1.Area > Q2.Area)
+                {
+                    return true;
+                }
+                else 
+                {
+                    return false;
+                }
+            }
+
+        //Overload del Segno "<"
+        public static bool operator <(Q Q1, Q Q2)
+        {
+            if (Q1.Area < Q2.Area)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ bertozzi.mattia.4h.Quadrato/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using bertozzi.mattia._4h.Quadrato.Models;
+
+namespace bertozzi.mattia._4h.Quadrato
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Programma Quadrato di Mattia Bertozzi");
+
+            //10,5 sono i valori x e y del punto, 25 è il lato
+            Q Q1 = new Q(10,5,25);
+
+            Console.WriteLine(Q1.OutPunto());
+
+            Q Q2 = new Q(0,0,15);
+
+            Console.WriteLine("Dati Quadrato Q1");
+            Console.WriteLine($"\tArea:\t\t{Q1.CArea()}");
+            Console.WriteLine($"\tPerimetro:\t{Q1.CPerimetro()}");
+            Console.WriteLine($"\tDiagonale:\t{Q1.CDiagonale()}");
+
+            Console.WriteLine("Dati Quadrato Q2");
+            Console.WriteLine($"\tArea:\t\t{Q2.CArea()}");
+            Console.WriteLine($"\tPerimetro:\t{Q2.CPerimetro()}");
+            Console.WriteLine($"\tDiagonale:\t{Q2.CDiagonale()}");
+
+            Console.WriteLine("Eseguo Confronto tra i Quadrati");
+
+            if (Q1>Q2)
+            {
+                Console.WriteLine("il Quadrato Q1 è più grande");
+            }
+            else
+            {
+                Console.WriteLine("il Quadrato Q2 è più grande");
+            }
+
+        }
+    }
+}
